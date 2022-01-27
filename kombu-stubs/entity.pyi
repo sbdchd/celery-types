4bcaa66bc437ec9e2e62406230d9db4f8a8d3048
Peteris Ratnieks <peteris.ratnieks@zealid.com>
2022-01-27 15:10:17 +0200

    improve kombu types

--- kombu-stubs/entity.pyi
+++ kombu-stubs/entity.pyi
@@ -1,4 +1,25 @@
 from kombu.abstract import MaybeChannelBound
+from kombu.transport.base import Channel
+from typing import Any, Optional
 
-class Exchange(MaybeChannelBound): ...
-class Queue(MaybeChannelBound): ...
+class Exchange(MaybeChannelBound):
+    def __init__(
+        self,
+        name: str ='',
+        type: str='',
+        channel: Optional[Channel]=None,
+        **kwargs: Any,
+    ) -> None: ...
+
+
+class Queue(MaybeChannelBound):
+    def __init__(
+        self,
+        name: str='',
+        exchange: Optional[Exchange]=None,
+        routing_key: str='',
+        channel: Optional[Channel]=None,
+        bindings: Any=None,
+        on_declared: Any=None,
+        **kwargs: Any,
+    ) -> None: ...
