2e9146b7d69194a3682f9a2361b7429ceba4e8eb
Steve Dignam <steve@dignam.xyz>
2022-01-27 19:08:03 -0500

    fix fmt

--- kombu-stubs/entity.pyi
+++ kombu-stubs/entity.pyi
@@ -1,25 +1,25 @@
+from typing import Any, Optional
+
 from kombu.abstract import MaybeChannelBound
 from kombu.transport.base import Channel
-from typing import Any, Optional
 
 class Exchange(MaybeChannelBound):
     def __init__(
         self,
-        name: str ='',
-        type: str='',
-        channel: Optional[Channel]=None,
+        name: str = ...,
+        type: str = ...,
+        channel: Optional[Channel] = ...,
         **kwargs: Any,
     ) -> None: ...
-
 
 class Queue(MaybeChannelBound):
     def __init__(
         self,
-        name: str='',
-        exchange: Optional[Exchange]=None,
-        routing_key: str='',
-        channel: Optional[Channel]=None,
-        bindings: Any=None,
-        on_declared: Any=None,
+        name: str = ...,
+        exchange: Optional[Exchange] = ...,
+        routing_key: str = ...,
+        channel: Optional[Channel] = ...,
+        bindings: Any = ...,
+        on_declared: Any = ...,
         **kwargs: Any,
     ) -> None: ...
