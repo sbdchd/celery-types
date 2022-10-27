06868f00270daf91948eeada7594e2087526b0bc
Petter Friberg <petter_friberg@hotmail.com>
2022-10-27 22:54:35 +0200

    Don't lose function signature when decorated with Signal.connect

--- celery-stubs/utils/dispatch/signal.pyi
+++ celery-stubs/utils/dispatch/signal.pyi
@@ -1,11 +1,22 @@
-from typing import Any, Callable, Optional, TypeVar
+from typing import Any, Callable, Optional, TypeVar, overload
 
 _F = TypeVar("_F", bound=Callable[..., Any])
 
 class Signal:
+    @overload
     def connect(
         self,
-        receiver: Callable[..., Any] = ...,
+        receiver: _F,
+        sender: Optional[Any] = ...,
+        weak: bool = ...,
+        dispatch_uid: str = ...,
+        retry: bool = ...,
+    ) -> _F: ...
+
+    @overload
+    def connect(
+        self,
+        *,
         sender: Optional[Any] = ...,
         weak: bool = ...,
         dispatch_uid: str = ...,
