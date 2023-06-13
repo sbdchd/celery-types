30364c7d021ef5da72e1ade8175a4f1854d0306d
Joey Orlando <joseph.t.orlando@gmail.com>
2023-06-13 08:12:37 +0200

    fix linting errors

--- celery-stubs/utils/time.pyi
+++ celery-stubs/utils/time.pyi
@@ -1,10 +1,8 @@
-from typing import Optional
-
 class ffwd: ...
 
 def get_exponential_backoff_interval(
     factor: int,
     retries: int,
     maximum: int,
-    full_jitter: Optional[bool],
+    full_jitter: bool | None,
 ) -> int: ...
