35e87e9bbf3366c8ce5a6d8988daf93f0a78fb95
Alessio Bogon <778703+youtux@users.noreply.github.com>
2024-09-02 17:20:47 +0200

    Add misc missing types (#163)
    This PR adds a few missing types:
    * `ColorFormatter`
    * `get_current_task`
    
    and it fixes the definition of `Context.headers` to be a dict (mutable)

--- celery-stubs/utils/log.pyi
+++ celery-stubs/utils/log.pyi
@@ -1,4 +1,7 @@
-from logging import Logger
+from logging import Formatter, Logger
+from typing import ClassVar
+
+from celery.utils.term import colored
 
 class LoggingProxy: ...
 
@@ -7,3 +10,10 @@ def get_task_logger(name: str) -> Logger: ...
 
 task_logger: Logger
 worker_logger: Logger
+
+class ColorFormatter(Formatter):
+    use_color: bool
+
+    COLORS: ClassVar[dict[str, colored]]
+    colors: ClassVar[dict[str, colored]]
+    def __init__(self, fmt: str | None = ..., use_color: bool = ...) -> None: ...
