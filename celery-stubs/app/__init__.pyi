96fd2799b7a74c151e84f9cfc37238a28ea77231
Stephen Eisenhauer <bhs2007@gmail.com>
2025-08-29 16:41:45 -0700

    Use name `**options` for consistency with other task decorator

--- celery-stubs/app/__init__.pyi
+++ celery-stubs/app/__init__.pyi
@@ -131,5 +131,5 @@ def shared_task(
     request_stack: _LocalStack[Context] = ...,
     abstract: bool = ...,
     queue: str = ...,
-    **kwargs: Any,
+    **options: Any,
 ) -> Callable[[Callable[..., Any]], _T]: ...
