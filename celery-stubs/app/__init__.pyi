sbdchd/celery-types

commit 410008fdd0b9b1c0c26407e2dae317ca8077e1dd

chore: allow lists in autoretry_for (#188)
According to celery's [docs](https://docs.celeryq.dev/en/stable/userguide/tasks.html#Task.autoretry_for) `autoretry_for` and `dont_autoretry_for` accept both lists and tuples.

In this PR we change the types from `tuple` to `Sequence` as that would allow to accept both `tuple`s and `list`s. This should be relatively safe as celery is not mutating that value, and in fact it should work with any iterable. See https://github.com/celery/celery/blob/7c75fa738885315180f0194da04cf5105994fe13/celery/app/autoretry.py#L10-L13

Fixes #187.

celery-stubs/app/__init__.pyi
@@ -1,4 +1,4 @@
-from collections.abc import Callable
+from collections.abc import Callable, Sequence
 from datetime import datetime
 from typing import (
     Any,
@@ -28,8 +28,8 @@ def shared_task(
     name: str = ...,
     serializer: str = ...,
     bind: Literal[False] = ...,
-    autoretry_for: tuple[type[BaseException], ...] = ...,
-    dont_autoretry_for: tuple[type[BaseException], ...] = ...,
+    autoetry_for: Sequence[type[BaseException]] = ...,
+    dont_autoetry_for: Sequence[type[BaseException]] = ...,
     max_retries: int | None = ...,
     default_retry_delay: int = ...,
     acks_late: bool = ...,
@@ -65,8 +65,8 @@ def shared_task(
     name: str = ...,
     serializer: str = ...,
     bind: Literal[True],
-    autoretry_for: tuple[type[BaseException], ...] = ...,
-    dont_autoretry_for: tuple[type[BaseException], ...] = ...,
+    autoetry_for: Sequence[type[BaseException]] = ...,
+    dont_autoetry_for: Sequence[type[BaseException]] = ...,
     max_retries: int | None = ...,
     default_retry_delay: int = ...,
     acks_late: bool = ...,
@@ -102,8 +102,8 @@ def shared_task(
     name: str = ...,
     serializer: str = ...,
     bind: bool = ...,
-    autoretry_for: tuple[type[BaseException], ...] = ...,
-    dont_autoretry_for: tuple[type[BaseException], ...] = ...,
+    autoetry_for: Sequence[type[BaseException]] = ...,
+    dont_autoetry_for: Sequence[type[BaseException]] = ...,
     max_retries: int | None = ...,
     default_retry_delay: int = ...,
     acks_late: bool = ...,

celery-stubs/app/base.pyi
@@ -106,8 +106,8 @@ class Celery:
         base: type[CeleryTask[Any, Any]] | None = ...,
         bind: bool = ...,
         # options
-        autoretry_for: tuple[type[BaseException], ...] = ...,
-        dont_autoretry_for: tuple[type[BaseException], ...] = ...,
+        autoetry_for: Sequence[type[BaseException]] = ...,
+        dont_autoetry_for: Sequence[type[BaseException]] = ...,
         retry_kwargs: dict[str, Any] = ...,
         retry_backoff: bool | int = ...,
         retry_backoff_max: int = ...,
@@ -153,8 +153,8 @@ class Celery:
         name: str = ...,
         serializer: str = ...,
         bind: bool = ...,
-        autoretry_for: tuple[type[BaseException], ...] = ...,
-        dont_autoretry_for: tuple[type[BaseException], ...] = ...,
+        autoetry_for: Sequence[type[BaseException]] = ...,
+        dont_autoetry_for: Sequence[type[BaseException]] = ...,
         max_retries: int | None = ...,
         default_retry_delay: int = ...,
         acks_late: bool = ...,
@@ -193,8 +193,8 @@ class Celery:
         name: str = ...,
         serializer: str = ...,
         bind: Literal[False] = ...,
-        autoretry_for: tuple[type[BaseException], ...] = ...,
-        dont_autoretry_for: tuple[type[BaseException], ...] = ...,
+        autoetry_for: Sequence[type[BaseException]] = ...,
+        dont_autoetry_for: Sequence[type[BaseException]] = ...,
         max_retries: int | None = ...,
         default_retry_delay: int = ...,
         acks_late: bool = ...,
@@ -233,8 +233,8 @@ class Celery:
         name: str = ...,
         serializer: str = ...,
         bind: Literal[True],
-        autoretry_for: tuple[type[BaseException], ...] = ...,
-        dont_autoretry_for: tuple[type[BaseException], ...] = ...,
+        autoetry_for: Sequence[type[BaseException]] = ...,
+        dont_autoetry_for: Sequence[type[BaseException]] = ...,
         max_retries: int = ...,
         default_retry_delay: int = ...,
         acks_late: bool = ...,

commit 9fe5ba8a0d6545e3e08a96c864b572a26d6d1b31

Fix typo in autoretry parameter (#190)
Just a fix for a typo introduced in #188

```diff
- autoetry
+ autoretry
```

celery-stubs/app/__init__.pyi
@@ -28,8 +28,8 @@ def shared_task(
     name: str = ...,
     serializer: str = ...,
     bind: Literal[False] = ...,
-    autoetry_for: Sequence[type[BaseException]] = ...,
-    dont_autoetry_for: Sequence[type[BaseException]] = ...,
+    autoretry_for: Sequence[type[BaseException]] = ...,
+    dont_autoretry_for: Sequence[type[BaseException]] = ...,
     max_retries: int | None = ...,
     default_retry_delay: int = ...,
     acks_late: bool = ...,
@@ -65,8 +65,8 @@ def shared_task(
     name: str = ...,
     serializer: str = ...,
     bind: Literal[True],
-    autoetry_for: Sequence[type[BaseException]] = ...,
-    dont_autoetry_for: Sequence[type[BaseException]] = ...,
+    autoretry_for: Sequence[type[BaseException]] = ...,
+    dont_autoretry_for: Sequence[type[BaseException]] = ...,
     max_retries: int | None = ...,
     default_retry_delay: int = ...,
     acks_late: bool = ...,
@@ -102,8 +102,8 @@ def shared_task(
     name: str = ...,
     serializer: str = ...,
     bind: bool = ...,
-    autoetry_for: Sequence[type[BaseException]] = ...,
-    dont_autoetry_for: Sequence[type[BaseException]] = ...,
+    autoretry_for: Sequence[type[BaseException]] = ...,
+    dont_autoretry_for: Sequence[type[BaseException]] = ...,
     max_retries: int | None = ...,
     default_retry_delay: int = ...,
     acks_late: bool = ...,

celery-stubs/app/base.pyi
@@ -106,8 +106,8 @@ class Celery:
         base: type[CeleryTask[Any, Any]] | None = ...,
         bind: bool = ...,
         # options
-        autoetry_for: Sequence[type[BaseException]] = ...,
-        dont_autoetry_for: Sequence[type[BaseException]] = ...,
+        autoretry_for: Sequence[type[BaseException]] = ...,
+        dont_autoretry_for: Sequence[type[BaseException]] = ...,
         retry_kwargs: dict[str, Any] = ...,
         retry_backoff: bool | int = ...,
         retry_backoff_max: int = ...,
@@ -153,8 +153,8 @@ class Celery:
         name: str = ...,
         serializer: str = ...,
         bind: bool = ...,
-        autoetry_for: Sequence[type[BaseException]] = ...,
-        dont_autoetry_for: Sequence[type[BaseException]] = ...,
+        autoretry_for: Sequence[type[BaseException]] = ...,
+        dont_autoretry_for: Sequence[type[BaseException]] = ...,
         max_retries: int | None = ...,
         default_retry_delay: int = ...,
         acks_late: bool = ...,
@@ -193,8 +193,8 @@ class Celery:
         name: str = ...,
         serializer: str = ...,
         bind: Literal[False] = ...,
-        autoetry_for: Sequence[type[BaseException]] = ...,
-        dont_autoetry_for: Sequence[type[BaseException]] = ...,
+        autoretry_for: Sequence[type[BaseException]] = ...,
+        dont_autoretry_for: Sequence[type[BaseException]] = ...,
         max_retries: int | None = ...,
         default_retry_delay: int = ...,
         acks_late: bool = ...,
@@ -233,8 +233,8 @@ class Celery:
         name: str = ...,
         serializer: str = ...,
         bind: Literal[True],
-        autoetry_for: Sequence[type[BaseException]] = ...,
-        dont_autoetry_for: Sequence[type[BaseException]] = ...,
+        autoretry_for: Sequence[type[BaseException]] = ...,
+        dont_autoretry_for: Sequence[type[BaseException]] = ...,
         max_retries: int = ...,
         default_retry_delay: int = ...,
         acks_late: bool = ...,