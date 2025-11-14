8e786eae68fb663c34150f58faf9db02c643c664
Oliver Haas <ohaas@e1plus.de>
2025-11-14 22:57:31 +0100

    chore: add saferepr and reprstream function signatures to utils/saferepr.pyi

---
+++ celery-stubs/utils/saferepr.pyi
@@ -0,0 +1,14 @@
+from collections import deque
+from collections.abc import Callable, Iterator
+from typing import Any
+
+__all__: tuple[str, str]
+
+def saferepr(o: Any, maxlen: int | None = ..., maxlevels: int = ..., seen: set[int] | None = ...) -> str: ...
+def reprstream(
+    stack: deque[Any],
+    seen: set[int] | None = ...,
+    maxlevels: int = ...,
+    level: int = ...,
+    isinstance: Callable[[Any, Any], bool] = ...,
+) -> Iterator[tuple[Any, Any]]: ...
