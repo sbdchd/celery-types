e5362cca335e49d3d71c5099cef2c9456f85e2d5
Denis Diveev <ddiveev99@gmail.com>
2025-12-18 01:46:58 +0300

    feat: add type stubs for app, manager, mocks, tasks, and worker modules

---
+++ celery-stubs/contrib/testing/mocks.pyi
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+from collections.abc import Callable, Mapping, Sequence
+from typing import Any
+from unittest.mock import Mock
+
+from celery import Celery
+from celery.canvas import Signature
+from typing_extensions import Self
+
+def TaskMessage(
+    name: str,
+    id: str | None = None,
+    args: Sequence[Any] = (),
+    kwargs: Mapping[Any, Any] | None = None,
+    callbacks: Sequence[Signature[Any]] | None = None,
+    errbacks: Sequence[Signature[Any]] | None = None,
+    chain: Sequence[Signature[Any]] | None = None,
+    shadow: str | None = None,
+    utc: bool | None = None,
+    **options: Any,
+) -> Any: ...
+def TaskMessage1(
+    name: str,
+    id: str | None = None,
+    args: Sequence[Any] = (),
+    kwargs: Mapping[Any, Any] | None = None,
+    callbacks: Sequence[Signature[Any]] | None = None,
+    errbacks: Sequence[Signature[Any]] | None = None,
+    chain: Sequence[Signature[Any]] | None = None,
+    **options: Any,
+) -> Any: ...
+def task_message_from_sig(
+    app: Celery,
+    sig: Signature[Any],
+    utc: bool = True,
+    TaskMessage: Callable[..., Any] = TaskMessage,
+) -> Any: ...
+
+class _ContextMock(Mock):
+    def __enter__(self) -> Self: ...
+    def __exit__(self, *exc_info: Any) -> None: ...
+
+def ContextMock(*args: Any, **kwargs: Any) -> _ContextMock: ...
