617724e1111ba2a04af648c0c2a18bf7b39aec57
Denis Diveev <ddiveev99@gmail.com>
2025-12-30 22:20:25 +0300

    Add type stubs for abortable, migrate, pytest, rdb, and sphinx modules

---
+++ celery-stubs/contrib/sphinx.pyi
@@ -0,0 +1,41 @@
+from typing import Any, Literal
+
+from celery.app.task import Task as BaseTask
+from docutils import nodes
+from sphinx.domains.python import PyFunction
+from sphinx.ext.autodoc import Documenter, FunctionDocumenter
+from typing_extensions import override
+
+class TaskDocumenter(FunctionDocumenter):
+    objtype: str = "task"
+    member_order: int = 11
+
+    @override
+    @classmethod
+    def can_document_member(
+        cls: type[Documenter],
+        member: Any,
+        membername: str,
+        isattr: bool,
+        parent: Any,
+    ) -> bool: ...
+    @override
+    def format_args(self) -> str: ...  # type: ignore[override]  # pyright: ignore[reportIncompatibleMethodOverride]
+    @override
+    def document_members(self, all_members: bool = False) -> None: ...
+    @override
+    def check_module(self) -> bool: ...
+
+class TaskDirective(PyFunction):
+    @override
+    def get_signature_prefix(self, sig: str) -> list[nodes.Node]: ...
+
+def autodoc_skip_member_handler(
+    app: Any,
+    what: object,
+    name: object,
+    obj: BaseTask[Any, Any],
+    skip: bool,
+    options: object,
+) -> Literal[False] | None: ...
+def setup(app: Any) -> dict[Literal["parallel_read_safe"], Literal[True]]: ...
